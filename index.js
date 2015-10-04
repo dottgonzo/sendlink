2f411d6e19ac69bf3269b0cd9d45167bcc6e078f
Dario Caruso <darioyzf@gmail.com>
2015-10-04 21:40:27 +0200

    config

--- index.js
+++ index.js
@@ -1,22 +1,61 @@
 var timerdaemon=require('timerdaemon'),
 verb=require('verbo'),
-PouchDB=require('pouchdb');
+PouchDB=require('pouchdb'),
+diff=require('deep-diff'),
+Promise = require('promise');
 
-module.exports = function(url,time) {
-if(!time){
-  time=300000;//5 minuti
+
+function overr(data,overrides){
+  diff.observableDiff(data, overrides, function (d) {
+      // Apply all changes except those to the 'name' property...
+      if (d.kind != 'D') {
+          diff.applyChange(data, overrides, d);
+      }
+  });
 }
+
+
+module.exports = function(url,json) {
+
+
+
+if(!json||!json.time){
+  time=300000;//5 minuti
+} else{
+  time=json.time;//5 minuti
+}
+
+
   timerdaemon.pre(time,function(){
+
+    if(json&&json.over){
+      over=json.over;
+    } else{
+      over={};
+    }
+
+    if(!over._id){
+      over._id="linkstatus";
+    }
+
+
     var remoteDB=PouchDB(url);
-        remoteDB.get('linkstatus').then(function(doc){
-          doc.updatedAt=new Date().getTime();
-          remoteDB.put(doc);
+
+
+        remoteDB.get(over._id).then(function(doc){
+          over.updatedAt=new Date().getTime();
+          remoteDB.put(over);
           verb("new document","info","updating the state");
         }).catch(function(err){
-          console.log(err);
+
           if (err.status==404) {
-            verb("new document","warn","Getting document");
-            remoteDB.put({_id:"linkstatus",dbtype:"linkstatus","updatedAt":new Date().getTime()})
+            over.updatedAt=new Date().getTime();
+            remoteDB.put(over).catch(function(err){
+              verb("cannot put","error","Sendlink")
+            }).catch(function(err){
+              verb(err,"error","Sendlink")
+
+            })
           } else{
             verb("can't get the document","error","Getting document");
           }
--- tests/main.js
+++ tests/main.js
@@ -1,2 +1,15 @@
-var sendlink=require('../index.js');
-sendlink('nowdb')
+var sendlink=require('../index.js'),
+verb=require('verbo'),
+PouchDB=require('pouchdb'),
+timerdaemon=require('timerdaemon'),
+testdb='testdb';
+
+sendlink(testdb,{time:5000,over:{_id:"sggss"}});
+
+timerdaemon.pre(5000,function(){
+
+  PouchDB(testdb).allDocs({include_docs:true}).then(function(docs){
+    verb(JSON.stringify(docs))
+  })
+
+})
